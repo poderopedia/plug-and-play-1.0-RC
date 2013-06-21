565f2adeb0ef21c63d1f9a4bbbcec55b0fbcdfa2
Juan Eduardo <juaneduardohv@gmail.com>
2013-06-21 12:53:05 -0300

    Update 0_memcached.py

--- poderopedia/models/0_memcached.py
+++ poderopedia/models/0_memcached.py
@@ -1,3 +1,6 @@
-__author__ = 'Evolutiva'
+from gluon.contrib.memcache import MemcacheClient
+memcache_servers = ['127.0.0.1:11211']
+cache.memcache = MemcacheClient(request, memcache_servers)
+cache.ram = cache.disk = cache.memcache
 
 
